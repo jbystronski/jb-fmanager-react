8c976ebddff4888699f6c9d42ef6abd816f8d95a
Jakub Bystroński <jbstr85@gmail.com>
2022-11-04 22:17:27 +0100

    Reformed rollup.config.js, package.json

--- rollup.config.js
+++ rollup.config.js
@@ -4,63 +4,53 @@ import babel from "@rollup/plugin-babel";
 import css from "rollup-plugin-import-css";
 import externals from "rollup-plugin-node-externals";
 import alias from "@rollup/plugin-alias";
+import { terser } from "rollup-plugin-terser";
+
 const path = require("path");
-
-const packageJson = require("./package.json");
-
-const aliasResolve = (target) => path.resolve(__dirname, target);
+const pkgJson = require("./package.json");
 
 const devMode = process.env.NODE_ENV === "development";
 console.log(`${devMode ? "development" : "production"} mode bundle`);
 
 export default {
-  input: "./src/index.js",
   external: ["react", "react-dom", "styled-components"],
   watch: {
     include: "./src/**",
     clearScreen: false,
   },
-  output: {
-    file: packageJson.main,
-    format: "esm",
-    sourcemap: devMode ? "inline" : false,
-  },
+  input: "src/index.js",
+  output: [
+    {
+      file: pkgJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: pkgJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+  ],
   plugins: [
-    alias({
-      entries: [
-        {
-          find: "@ui",
-          replacement: aliasResolve("src/js/ui"),
-        },
-        {
-          find: "@providers",
-          replacement: aliasResolve("src/js/providers"),
-        },
-        {
-          find: "@utils",
-          replacement: aliasResolve("src/js/utils"),
-        },
-        {
-          find: "@helpers",
-          replacement: aliasResolve("src/js/helpers"),
-        },
-      ],
-    }),
     externals(),
-
-    css(),
-
-    resolve(),
     commonjs({
       ignoreGlobal: true,
       include: /\/node_modules\//,
-      namedExports: {
-        react: Object.keys(require("react")),
-        "react-is": Object.keys(require("react-is")),
-      },
     }),
+    alias({
+      entries: ["ui", "providers", "utils", "helpers"].map((entry) => {
+        return {
+          find: `@${entry}`,
+          replacement: path.resolve(__dirname, `src/js/${entry}`),
+        };
+      }),
+    }),
+
+    css(),
+    resolve(),
     babel({
       babelHelpers: "runtime",
+
       exclude: /^(.+\/)?node_modules\/.+$/,
       presets: ["@babel/preset-env", "@babel/preset-react"],
       extensions: [".js", ".jsx"],
